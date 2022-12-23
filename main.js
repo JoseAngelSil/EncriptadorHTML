0caaaed0de789fc6aa0f4d7af7134b36fd6a02e2
Jose Angel <joseangel413silva@gmail.com!>
2022-12-22 23:06:47 -0600

    add: Diseño basico de primer plana
    Se ingresa el diseño y funcionalidad basica para obtener el texto que
    ingresa, ademas ya solo falta la parte funcional

---
+++ main.js
@@ -0,0 +1,16 @@
+// Creacion de variables de selector para la entrada de texto
+const textIn = document.querySelector("input");
+const btnEncriptar = document.querySelector("#btn-encriptar");
+const btnDesEncriptar = document.querySelector("#btn-desencriptar");
+textIn.focus();
+
+function mostrarText(){
+    alert(textIn.value);
+}
+
+function cmd(){
+    console.log(textIn.value);
+}
+
+btnEncriptar.onclick = mostrarText;
+btnDesEncriptar.onclick = cmd;
