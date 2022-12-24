3a14c46710651692014fce4b0769c42190b53a71
Jose Angel <joseangel413silva@gmail.com!>
2022-12-24 01:56:10 -0600

    add: programa de encriptacion y desencriptacion
    Se agrega la caracteristica de encriptar y desencriptar, ademas se manda
    el resultado en el div para que pueda ser atendido.
    
    Se crea el btn de copy pero no funciona. es investigar mas sobre el tema

--- main.js
+++ main.js
@@ -2,15 +2,68 @@
 const textIn = document.querySelector("#InputText");
 const btnEncriptar = document.querySelector("#btn-encriptar");
 const btnDesEncriptar = document.querySelector("#btn-desencriptar");
+const btnCopy = document.getElementById("btn-copy");
 textIn.focus();
 
-function mostrarText(){
-    alert(textIn.value);
+const diccionario = {
+  a: "ai",
+  e: "enter",
+  i: "imes",
+  o: "ober",
+  u: "ufat",
+};
+
+function encriptar() {
+  let textEncrip = "";
+  let text = textIn.value;
+  if (textIn.value !== "") {
+    for (let i = 0; i <= text.length - 1; i++) {
+      const letter = text[i];
+      if (letter in diccionario) {
+        textEncrip += diccionario[letter];
+      } else {
+        textEncrip += letter;
+      }
+    }
+    return mostrarMensaje(textEncrip);
+  } else {
+    return mostrarMensaje(
+      "Error: campo vacio, llene el campo para Encriptarlo"
+    );
+  }
 }
 
-function cmd(){
-    console.log(textIn.value);
+function desencriptar() {
+  let mensajeInDesencrip = "";
+  let text = textIn.value;
+  if (text !== "") {
+    mensajeInDesencrip += text
+      .replaceAll("ai", "a")
+      .replaceAll("enter", "e")
+      .replaceAll("imes", "i")
+      .replaceAll("ober", "o")
+      .replaceAll("ufat", "u");
+      return mostrarMensaje(mensajeInDesencrip);
+  }else{
+    return mostrarMensaje("Error: campo vacio, llene el campo para Desencriptarlo");
+  }
 }
 
-btnEncriptar.onclick = mostrarText;
-btnDesEncriptar.onclick = cmd;
+function mostrarMensaje(texOut) {
+  let out = document.getElementById('salida');
+  out.innerHTML ='<p id="out">'+ texOut+'</p>'+
+  "<div id='cajaCopy'><button type='submit' class='btn' id='btn-copy'>Copiar</button></div>";
+}
+
+btnEncriptar.onclick = encriptar;
+btnDesEncriptar.onclick = desencriptar;
+btnCopy.addEventListener('click',()=>{
+    let textToCopy = document.getElementById('out').innerText;
+    if(navigator.clipboard) {
+        navigator.clipboard.writeText(textToCopy).then(() => {
+            alert('Copied to clipboard')
+        })
+    } else {
+        console.log('Browser Not compatible')
+    }
+});
